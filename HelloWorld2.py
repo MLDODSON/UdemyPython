6bb3d44e6de079080c5a5d08afd91e16c7d6b0fe
MLDODSON <mldodson@fullsail.edu>
2017-08-18 12:44:05 -0500

    first lesson

---
+++ HelloWorld2.py
@@ -0,0 +1,12 @@
+#Getting started
+print("The Mothership Connection has landed!")
+print(2000 + 17)
+print("Is the year Funkateers.")
+print("Long live Dr. Funkenstein!")
+print("He who rules Parliament and " + "Funkadelic.")
+#This is how to use a variable in Python
+greeting = "What's up"
+name = "Bootsy?"
+print(greeting + ' ' + name)
+ask = "Play me some funky bass!"
+print(ask)
