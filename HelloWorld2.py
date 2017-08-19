f2c444b8ccebe09353cd70d5126edd0e3c20d88f
MLDODSON <mldodson@fullsail.edu>
2017-08-18 20:21:42 -0500

    variables, string splits and tabs

--- HelloWorld2.py
+++ HelloWorld2.py
@@ -1,12 +1,26 @@
 #Getting started
-print("The Mothership Connection has landed!")
-print(2000 + 17)
-print("Is the year Funkateers.")
-print("Long live Dr. Funkenstein!")
-print("He who rules Parliament and " + "Funkadelic.")
+#print("The Mothership Connection has landed!")
+#print(2000 + 17)
+#print("Is the year Funkateers.")
+#print("Long live Dr. Funkenstein!")
+#print("He who rules Parliament and " + "Funkadelic.")
 #This is how to use a variable in Python
-greeting = "What's up"
-name = "Bootsy?"
-print(greeting + ' ' + name)
-ask = "Play me some funky bass!"
-print(ask)
+#greeting = "What's up"
+#name = "Bootsy?"
+#print(greeting + ' ' + name)
+#ask = "Play me some funky bass!"
+#print(ask)
+
+#greeting = "Hello"
+#name = input("Please enter your name.")
+#print(greeting + ' ' + name)
+
+#This is how you split a string
+splitString = "The Mothership has\nlanded in St. Louis\nladies and gentlemen."
+print(splitString)
+
+#This is a tabbed string
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+
+print("Dr. Funkenstein said \"Make my funk the p funk. I want to get funk up!\"")
