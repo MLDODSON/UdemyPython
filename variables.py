79d8046d5618d4a7f8291184be8642e1a61fc6de
MLDODSON <mldodson@fullsail.edu>
2017-08-19 10:53:14 -0500

    strings and arrays

---
+++ variables.py
@@ -0,0 +1,63 @@
+#a = 12
+#b = 3
+#print(a + b)
+#print(a - b)
+#print(a * b)
+#print(a / b)
+# An / will give you a whole number of i.e. 4.0
+#print(a // b)
+# A // will give you an interger number of i.e. 4
+#print(a % b)
+
+#for i in range(1, 4):
+    #print(i)
+
+#for i in range(1, a//b):
+    #// is used to get interger numbers
+    #print(i)
+
+#x = 9
+#y = 3
+#z = 2
+#print(x // y)
+
+#print(x * y + y)
+
+#print(x // y + z)
+
+#print(z * y + x)
+
+#print(x * z - y)
+
+city = "Puerto Plata"
+print(city)
+
+print(city[4])
+
+print(city[-1]
+      )
+#This arrays prints the first 6 characters only
+print(city[0:6])
+
+#This arrays omitss the first 5 characters
+print(city[5:])
+
+print(city[0:5:2])
+
+#This arrays starts with the first character and extracts every 3rd one
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+
+#This arrays starts with the first character and extracts every 2nd one
+place = "Puerto Plata"
+print(place[0::2])
+
+#Examples of simple math with a python string
+print("Don't stop til you get enough! " * 4)
+print("Don't stop til you get enough! " * (6 + 4))
+
+#Checking for a sub string
+today = "Saturday"
+print("Sat" in today)
+print("lol" in today)
+#It's true that "Sat" is a sub string of today variable
